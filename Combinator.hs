a10c16c51a33272d2d429c59853882e71b18e0b8
Alex Bradley <a.bradley.dm@gmail.com>
2020-06-14 02:09:46 +0100

    Deleted temporary files

--- Combinator.hs
+++
@@ -1,8 +0,0 @@
-
-module AbLib.Parser.Combinator where
-
-char :: Parse s Char => Parser s Char
-char = parser
-
-match :: (Parse s t, Eq t) => t -> Parser s t
-match t = parser >>= guard . (t ==) >> return t
