3caf85a197d458348cd6ecf5f5122f061300168d
Alex Bradley <a.bradley.dm@gmail.com>
2023-06-07 19:32:09 +0100

    Some sample parsers and combinators

--- Control/Combinators.hs
+++ Control/Combinators.hs
@@ -5,7 +5,7 @@ import YAMP.Data.Stream
 
 import Data.List (sortOn)
 
--- Repeat a parsers exactly n times.
+-- Repeat a parser exactly n times.
 exactly :: (Monad m, Alternative m) => Int -> Parser m t a -> Parser m t [a]
 exactly n = between (n,n)
 
@@ -15,4 +15,17 @@ between (a,b) f = do
    xs <- many f
    guard (a <= length xs && length xs <= b)
    return xs
+
+-- Matches the given value
+-- E.g. match 'x' or match "foobar"
+match :: (Parse t a, Eq a, MonadPlus m) => a -> Parser m t a
+match t = do
+   x <- parser
+   guard (t == x)
+   pure x
+
+-- Matches any of the given values
+-- E.g. matchAny ["foo", "bar"]
+matchAny :: (Parse t a, Eq a, MonadPlus m, Foldable f) => f a -> Parser m t a
+matchAny = foldMap match
 
---
+++ Control/Parsers.hs
@@ -0,0 +1,49 @@
+module YAMP.Control.Parsers where
+
+import YAMP.Data.Parser
+import YAMP.Data.Types
+import YAMP.Data.Stream (isEmpty, fromList)
+import Data.Char (isSpace)
+
+-- Matches a nonempty region of contiguous whitespace
+whitespace :: MonadPlus => Parser m Char String
+whitespace = some $ charThat isSpace
+
+-- Matches a nonempty region of contiguous horizontal whitespace
+hspace :: MonadPlus m => Parser m Char String
+hspace = some $ matchAny "\9\32\160\5760\8192\8193\8194\8195\8196\8197\8198\8199\8200\8201\8202\8239\8287\12288"
+
+-- Matches a nonempty region of contiguous vertical whitespace (newlines)
+vspace :: MonadPlus m => Parser m Char String
+vspace = some $ matchAny "\10\11\12\13\133\8232\8233"
+
+-- Matches a letter
+letter :: MonadPlus m => Parser m Char Char
+letter = letter_upper <|> letter_lower
+
+-- Matches an uppercase letter
+letter_upper :: MonadPlus m => Parser m Char Char
+letter_upper = matchAny ['A'..'Z']
+
+-- Matches an lowercase letter
+letter_lower :: MonadPlus m => Parser m Char Char
+letter_lower = matchAny ['a'..'z']
+
+-- Matches a digit
+digit :: MonadPlus m => Parser m Char Char
+digit = matchAny ['0'..'9']
+
+-- Matches an alphanumeric character
+alphanum :: MonadPlus m => Parser m Char Char
+alphanum = letter <|> digit
+
+-- Matches a normal identifier format
+ident :: MonadPlus m => Parser m Char String
+ident = liftA2 (:) letter $ many (alphanum <|> match '_')
+
+-- Matches end of input
+eof :: MonadPlus m => Parser m Char ()
+eof = do
+   mapInput (\s -> if isEmpty s then fromList "\3" else s)
+   char '\3'
+   pure ()
